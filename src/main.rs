5e82dc6725c6e365da74d0f2ed8316a4a0680c52
Brian Herman <brianherman@gmail.com>
2016-08-14 18:26:10 -0500

    made the function work betta

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,28 @@
  */
 extern crate rustc_serialize as serialize;
 use serialize::hex::FromHex;
+use serialize::base64::{self, ToBase64};
 fn main(){
-  let test = xor("1c","2c");
+  let test = xor("111111","ffffff");
   for x in test{
     println!("{}", x);
   }
 }
 fn xor(input: &str, byte: &str)-> Vec<u8>{
+    if input.len() != byte.len(){
+        return vec![0];
+    }
     let input2hex = input.from_hex().unwrap(); 
     let byte2hex = byte.from_hex().unwrap();
     let mut xor = Vec::new();
-    for i in 0..input.len(){
-        xor.push(input2hex[i] ^ byte2hex[i]);
+    for it in input2hex.iter().zip(byte2hex.iter()){
+        let (ai, bi) = it;
+        xor.push(ai ^ bi);
     }
     return xor;
 }
 
+
+fn base64(input: &str) -> String{
+    return input.from_hex().unwrap().to_base64(base64::STANDARD);
+}
